a73c4228c3c065253da6b7cad1089b313f51e75d
bjnepali7 <109470940+bjnepali7@users.noreply.github.com>
2025-02-11 20:33:12 +0545

    Add files via upload

---
+++ var_and_dynamic.dart
@@ -0,0 +1,15 @@
+void main(){
+
+  var section=11;
+  //section="ee" this will bw  error because if we dont know what data to put and we put data and  the data we put  in variable will become that data type
+  section =10;// ovveride of the data
+   var roll;// this  is a dynamic way
+  // if we want to constantlyu put different datatype then it is used
+   roll="e";
+   roll=10;
+   dynamic b;
+  
+
+
+
+}
---
+++ varanddatatype.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main(){
+  BigInt longvalue;// big int is used to store big integer value
+  longvalue=BigInt.parse("75283568273658273658237562387562378562378");
+  print(longvalue);
+
+  num a=8.59;//num datatype is used to  store float and integer data type
+  num b=8;
+print(a);
+print(b);
+}
