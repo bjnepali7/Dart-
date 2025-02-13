4e80b6e2def5c39c0ade21c06800af98afdbac3c
bjnepali7 <109470940+bjnepali7@users.noreply.github.com>
2025-02-13 19:26:29 +0545

    Add files via upload

---
+++ functions.dart
@@ -0,0 +1,11 @@
+
+import 'dart:io';
+
+int addNumber(int a,int b){//function which return  integer
+  return a+b;
+}
+void main(){
+  //addNumber(5, 7);
+  print("the sum is:${addNumber(4, 6)}");
+stdout.write("the sum is :${addNumber(7, 8)}");
+}
