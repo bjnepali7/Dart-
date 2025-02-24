62f12d2517dc25b31c968bfd1db0f8789950d17b
bjnepali7 <109470940+bjnepali7@users.noreply.github.com>
2025-02-24 17:49:23 +0545

    added txt

--- clssobjexmple.dart
+++ clssobjexmple.dart
@@ -1,11 +1,12 @@
- class dog{
-   void bark(){
-     print("bark");
-   }
+class dog {
+  void bark() {
+    print("bark");
+  }
+}
 
- }
- void main(){
-  dog h= dog();//in new flutter dart we can create object without the new keyword
-
+void main() {
+  dog h =
+      dog(); //in new flutter dart we can create object without the new keyword
+//hello
   h.bark();
- }
+}
